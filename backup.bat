b4a255c8495b10b3fbe22fa7c1d581063fca4918
Marcelo Flores <omarcelo.0101@gmail.com>
2024-05-30 13:55:42 -0300

    arquivo .bat para backup automatico do mongo na pasta selecionada

---
+++ backup.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set caminho_backup=C:\mongobackup
+set caminho_mongo_tool=C:\Program Files\MongoDB\Tools\100\bin
+set nome_arquivo=backup-%date:/=-%
+
+if not exist "%caminho_backup%" (
+    mkdir "%caminho_backup%"
+)
+
+cd /d "%caminho_backup%"
+
+"%caminho_mongo_tool%\mongodump" --gzip --archive=%nome_arquivo%.archive
+
+if %errorlevel% neq 0 (
+    echo MongoDB backup falhou. abortando script.
+    pause
+    exit /b 1
+)
+
+set substring=%nome_arquivo%
+
+for /f "delims=" %%f in ('dir /b /a-d') do (
+    if "%%~nf" neq "!substring!" (
+        echo deletando arquivo: %%f
+        del "%%f"
+    )
+)
+
+endlocal
