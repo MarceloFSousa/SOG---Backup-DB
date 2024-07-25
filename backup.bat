461ca577edaa75e75536a6502209efbd145930a1
Marcelo Flores <omarcelo.0101@gmail.com>
2024-07-25 11:26:02 -0300

    adicionando opcao de deletar outros backups ou nao

--- backup.bat
+++ backup.bat
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 set caminho_backup=C:\mongobackup
 set caminho_mongo_tool=C:\Program Files\MongoDB\Tools\100\bin
 set nome_arquivo=backup-%date:/=-%
+set deletar_backups_antigos=false
 
 if not exist "%caminho_backup%" (
     mkdir "%caminho_backup%"
@@ -19,12 +20,14 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-set substring=%nome_arquivo%
+if "%deletar_backups_antigos%" == "true" (
+   set substring=%nome_arquivo%
 
-for /f "delims=" %%f in ('dir /b /a-d') do (
-    if "%%~nf" neq "!substring!" (
-        echo deletando arquivo: %%f
-        del "%%f"
+    for /f "delims=" %%f in ('dir /b /a-d') do (
+        if "%%~nf" neq "!substring!" (
+            echo deletando arquivo: %%f
+            del "%%f"
+        )
     )
 )
 
